c796bfbcd933bda27bf79e3c84ab02d03ab73434
clvinning <clvinning@valdosta.edu>
2016-01-12 00:31:05 -0500

    Finally found a carousel that I liked.
    Source: https://github.com/vinayakjadhav/jR3DCarousel

--- js/app.js
+++ js/app.js
@@ -6,10 +6,26 @@ $(document).ready(function () {
     
     $(this).addClass("grow");
   */
-  $(".overlay").on("mouseenter", function() {
+  var slides = [
+    {src: 'http://lorempixel.com//1366/768'},
+    {src: 'http://lorempixel.com//1366/761'},
+    {src: 'http://lorempixel.com//1366/762'},
+    {src: 'http://lorempixel.com//1366/763'},
+    {src: 'http://lorempixel.com//1366/764'},
+    {src: 'http://lorempixel.com//1366/765'},
+    {src: 'http://lorempixel.com//1366/766'}
+  ];
+  
+  $(".overlay").on("mouseenter", function () {
     $(this).addClass("zoom-box");
   });
-  $(".products-wrapper section").on("mouseleave", function() {
+  
+  $(".products-wrapper section").on("mouseleave", function () {
     $(".overlay").removeClass("zoom-box");
   });
+  
+  $('.jR3DCarouselGallery').jR3DCarousel({
+    slides: slides
+  });
+  
 });
